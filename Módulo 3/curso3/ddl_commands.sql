8657f7f9d4c3827639c82eff14e8f3061bda0890
Fernando DT Schmitz <132524731+fdtschmitz@users.noreply.github.com>
2023-12-05 05:34:39 -0300

    Update ddl_commands.sql
    Adjust in some query.

--- Módulo 3/curso3/ddl_commands.sql
+++ Módulo 3/curso3/ddl_commands.sql
@@ -13,17 +13,33 @@ CREATE TABLE bankAccounts (
 	CONSTRAINT identification_account_constraint UNIQUE (Ag_num, Ac_num)
 );
 
+alter table bankAccounts add LimiteCredito float
+	not null default 500.00;
+
+alter table bankAccounts add email varchar(60);
+alter table bankAccounts drop email;
+    
+desc bankAccounts;
+
+insert into bankAccounts (Ag_num, Ac_num, Saldo) values (156, 264358, 0);
+select * from bankAccounts;
+
 CREATE TABLE bankClient(
-	Id_client INT auto_increment PRIMARY KEY,
-	ClientAccount INT PRIMARY KEY,
+	Id_client INT auto_increment,
+	ClientAccount INT,
 	CPF VARCHAR(11) NOT NULL,
 	RG VARCHAR(9) NOT NULL,
 	Nome VARCHAR(50) NOT NULL,
 	Endereço VARCHAR(100) NOT NULL,
 	Renda_mensal FLOAT,
+    primary key (Id_client, ClientAccount),
 	CONSTRAINT fk_acconut_client foreign key (ClientAccount) REFERENCES bankAccounts(Id_account) 
 	ON UPDATE CASCADE
 );
+insert into bankClient (ClientAccount, CPF, RG, Nome, Endereço, Renda_mensal) values(1, 12345678913, 123456789, 'Fulano', 'rua de lá', 6500.6);
+select * from bankClient;
+alter table bankClient add UFF char(2) not null default 'RJ';
+update bankClient set UFF='MG' where Id_client = 1;
 
 CREATE TABLE bankTransactions(
 	Id_transcation INT auto_increment PRIMARY KEY,
