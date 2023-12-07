fa7774db5c24c0a49883a3b366d0a151d26ecce2
Fernando DT Schmitz <132524731+fdtschmitz@users.noreply.github.com>
2023-12-07 05:28:58 -0300

    Create Exploring Queries.sql

---
+++ Módulo 3/curso3/Exploring Queries.sql
@@ -0,0 +1,22 @@
+create database teste;
+use teste;
+
+create table R(
+	a char(2)
+    );
+create table S(
+	a char(2)
+    );
+    
+insert into R(A) values('a1'),('a2'),('a2'),('a3');
+insert into S(A) values('a1'),('a1'),('a2'),('a3'),('a4'),('a5');
+
+-- except -> not in
+select * from S where A not in (select A from R);
+
+-- intersect
+select R.A from R where R.A in(select S.A from S);
+
+-- union
+(select distinct R.A from R) UNION (select distinct S.A from S);
+(select R.A from R) UNION (select S.A from S);
